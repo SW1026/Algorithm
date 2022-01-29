d26945dbe38d4a9614e5f70b4c22b936f710c97d
seowoo.kim <39516757+SW1026@users.noreply.github.com>
2022-01-29 22:06:42 +0900

    Create 1181 단어 정렬.py

---
+++ 기타 구현/1181 단어 정렬.py
@@ -0,0 +1,13 @@
+n = int(input())
+a = []
+for _ in range(n):
+    tmp = input()
+    a.append([tmp, len(tmp)])
+
+a = list(set(map(tuple, a)))
+a.sort(key=lambda x: (x[1], x[0]))
+for i in a:
+    print(i[0])
+    
+    
+# https://www.acmicpc.net/problem/1181
