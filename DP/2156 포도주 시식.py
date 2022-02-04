2678a3e800a295cc0c3e1d7a2fd29e71a5e88b40
seowoo.kim <39516757+SW1026@users.noreply.github.com>
2022-02-05 01:20:43 +0900

    Update 2156 포도주 시식.py

--- DP/2156 포도주 시식.py
+++ DP/2156 포도주 시식.py
@@ -5,8 +5,17 @@ a = [int(input()) for _ in range(n)]
 d = [0]*n
 
 d[0] = a[0]
+if n == 1:
+    print(d[0])
+    exit(0)
 d[1] = a[0] + a[1]
+if n == 2:
+    print(d[1])
+    exit(0)
 d[2] = max(a[1]+a[2], a[0]+a[2], d[1])
+if n == 3:
+    print(d[2])
+    exit(0)
 for i in range(3, n):
     # 이전 포도주를 마셨냐 안 마셨냐, 지금 포도주를 먹지 않는 경우
     d[i] = max(d[i-3]+a[i-1]+a[i], d[i-2]+a[i], d[i-1])
