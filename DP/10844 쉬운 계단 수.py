3ce667504606990e8abc4e481384067eb7d25877
seowoo.kim <39516757+SW1026@users.noreply.github.com>
2022-02-05 14:47:47 +0900

    Create 10844 쉬운 계단 수.py

---
+++ DP/10844 쉬운 계단 수.py
@@ -0,0 +1,24 @@
+n = int(input())
+d = [[0] * 10 for _ in range(n+1)]
+
+# 초기 값 : d[1][0] 제외 나머지 1
+for i in range(1, 10):
+    d[1][i] = 1
+if n == 1:
+    print(9)
+    exit(0)
+
+for num in range(2, n+1):
+    d[num][0] = d[num-1][1]
+    d[num][9] = d[num-1][8]
+    for i in range(1, 9):
+        d[num][i] = d[num-1][i-1] + d[num-1][i+1]
+
+print(sum(d[n]) % 1000000000)
+
+
+# <DP가 막힐 때 TIP>
+# 다시 차근 차근 0부터 해보며 규칙을 발견해보자
+# 주어진 입력값 n을 기준으로 잡고(2차원 배열일 경우 행)
+# d[n] 또는 d[n][i]의 규칙을 생각하고 찾아나가기
+# 끈기있게 하기
